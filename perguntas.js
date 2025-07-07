27a9cdfac00519baaa7b5915e30e7c70d02cfb64
isabelaalves19 <146453954+isabelaalves19@users.noreply.github.com>
2025-07-07 08:42:00 -0300

    iiii

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia na moda ',
+    'Como a inteligência artificial pode ajudar a fazer roupas novas?',
+    'A inteligência artificial pode ajudar a criar novos estilos, escolher cores e até ajudar a montar looks que combinam bem.'
+)
+
+criaCartao(
+    'Moda e Tecnologia: O Futuro das Roupas Inteligentes',
+    'O que são roupas inteligentes?',
+    'Roupas inteligentes usam tecnologia, como sensores, para monitorar sua saúde ou ajustar a temperatura.',
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
